77c977f5b95b1ca19577f1ea7a6aab357204b143
Nikolay Rozhkov <rozhkov@uchi.ru>
2018-11-24 21:17:33 +0300

    Test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+# Mark.destroy_all
+Post.destroy_all
+User.destroy_all
+
+users_data = 10.times.map do
+  {
+    email: FFaker::Internet.email,
+    name: FFaker::Internet.user_name[0...16]
+  }
+end
+
+users = User.create! users_data
+
+User.first(7).each{ |user| user.update creator: true }
+User.first(2).each{ |user| user.update moderator: true }
+
+creators = User.where(creator: true)
+
+posts_data = 30.times.map do
+  {
+    user: creators.sample,
+    title: FFaker::Lorem.paragraph[0...100],
+    body: FFaker::Lorem.paragraphs.join[0...5_000]
+  }
+end
+
+posts = Post.create! posts_data
+
+comments_data = 150.times.map do
+  {
+    user: users.sample,
+    post: posts.sample,
+    body: FFaker::Lorem.paragraphs.join[0...500],
+    disactive: false
+  }
+end
+
+Comment.create! comments_data
+
+
+
+
