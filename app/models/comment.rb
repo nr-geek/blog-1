f8dc3ecc118571b1ae66c7a315a5588b3c495897
Nikolay Rozhkov <rozhkov@uchi.ru>
2018-11-24 18:40:30 +0300

    Validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :user, presence: true
+  validates :post, presence: true
+
+  validates :body, presence: true, length: { maximum: 500 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_many :comments
+
+  validates :user, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 5_000 }
 end
