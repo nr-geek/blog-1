ff74d5d751ae78799a636fc21ec91d147c969557
Nikolay Rozhkov <rozhkov@uchi.ru>
2018-11-24 19:54:23 +0300

    Some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 16, minimum: 2 }
   validates :email, presence: true
 
   has_many :comments
+  has_many :posts
 end
